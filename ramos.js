b7e7e167207ff942a4c8593a9ba051a14ec12e0e
Tessitoo <zmtessa@gmail.com>
2025-11-28 16:03:28 -0300

    Refactor Ramo constructor and draw method

--- ramos.js
+++ ramos.js
@@ -1,231 +1,242 @@
 var APPROVED = [];
 
-function Ramo(nombre, sigla, creditos, sector, prer=[], id, colorBySector) {
-	this.nombre = nombre;
-	this.sigla = sigla;
-	this.creditos = creditos;
-	this.sector = sector;
-	this.prer = new Set(prer);
-	this.id = id;
-	let approved = false;
-	let self = this;
-	let ramo;
+// Constructor de cada ramo
+function Ramo(nombre, sigla, creditos, sector, prer = [], id, colorBySector) {
+  this.nombre = nombre;
+  this.sigla = sigla;
+  this.creditos = creditos;
+  this.sector = sector;
+  this.prer = new Set(prer);
+  this.id = id;
 
-	this.draw = function(canvas, posX, posY, scaleX, scaleY) {
-		ramo = canvas.append('g')
-			.attr('id', self.sigla);
-		var sizeX = 100 * scaleX,
-			sizeY = 100 * scaleY
-		var graybar = sizeY/5;
+  let approved = false;
+  let self = this;
+  let ramo;
 
-		ramo.append("rect")
-			.attr("x", posX)
-			.attr("y", posY)
-			.attr("width", sizeX*1.2)
-			.attr("height", sizeY)
-			.attr("fill", colorBySector[sector][0]);
+  // Dibuja el ramo en el SVG
+  this.draw = function (canvas, posX, posY, scaleX, scaleY) {
+    ramo = canvas
+      .append("g")
+      .attr("id", self.sigla)
+      .attr("data-sector", self.sector); // para saber de qué tipo es este ramo
 
-		// above bar
-		ramo.append("rect")
-			.attr("x", posX)
-			.attr("y", posY)
-			.attr("width", sizeX*1.2)
-			.attr("height", graybar)
-			.attr("fill", '#6D6E71');
+    var sizeX = 100 * scaleX,
+      sizeY = 100 * scaleY;
+    var graybar = sizeY / 5;
 
-		// below bar
-		ramo.append("rect")
-			.attr("x", posX)
-			.attr("y", posY + sizeY - graybar)
-			.attr("width", sizeX*1.2)
-			.attr("height", graybar)
-			.attr("fill", '#6D6E71');
+    // Fondo principal del ramo
+    ramo
+      .append("rect")
+      .attr("x", posX)
+      .attr("y", posY)
+      .attr("width", sizeX * 1.2)
+      .attr("height", sizeY)
+      .attr("fill", colorBySector[self.sector][0])
+      .attr("class", "ramo-base"); // clase para luego cambiar color
 
-		// credits rect
-		ramo.append("rect")
-			.attr("x", posX + sizeX *1.2 - 23 * scaleX)
-			.attr("y", posY + sizeY - graybar + 1)
-			.attr("width", 19 * scaleX)
-			.attr("height", 18 * scaleY)
-			.attr("fill", 'white');
-		ramo.append("text")
-			.attr("x", posX + sizeX*1.2 - 17 * scaleX)
-			.attr("y", posY + sizeY - 6 * scaleY)
-			.text(self.creditos)
-			.attr("font-family", "sans-serif")
-			.attr("font-weight", "regular")
-			.attr("fill", "black")
-			.attr("font-size", 12 * scaleY);
-			
-		
-		ramo.append("text")
-			.attr("x", posX + sizeX*1.2/2)
-			.attr("y", posY + sizeY/2)
-			.text(self.nombre)
-			.attr("class", "ramo-label")
-			.attr("font-family", 'sans-serif')
-			.attr("fill", function() {
-				if (self.sector != 'IS')
-					return "white";
-				return '#6D6E71';
-			})
-			.attr("font-size", 13)
-			.attr("text-anchor", "middle")
-			.attr("dy", 0)
+    // Barra superior gris
+    ramo
+      .append("rect")
+      .attr("x", posX)
+      .attr("y", posY)
+      .attr("width", sizeX * 1.2)
+      .attr("height", graybar)
+      .attr("fill", "#6D6E71");
 
-		// Sigla
-		ramo.append("text")
-			.attr("x", posX + 2)
-			.attr("y", posY + sizeY/7)
-			.text(self.sigla)
-			.attr("font-family", "sans-serif")
-			.attr("font-weight", "bold")
-			.attr("fill", "white")
-			.attr("font-size", function() {
-				if (scaleX < 0.59)
-					return 9;
-				return 12;
-			});
+    // Barra inferior gris
+    ramo
+      .append("rect")
+      .attr("x", posX)
+      .attr("y", posY + sizeY - graybar)
+      .attr("width", sizeX * 1.2)
+      .attr("height", graybar)
+      .attr("fill", "#6D6E71");
 
-		ramo.append("rect")
-			.attr("x", posX)
-			.attr("y", posY)
-			.attr("width", sizeX*1.2)
-			.attr("height", sizeY)
-			.attr("fill", 'white')
-			.attr("opacity", "0.001")
-			.attr("class", "non-approved");
+    // Rectángulo de créditos
+    ramo
+      .append("rect")
+      .attr("x", posX + sizeX * 1.2 - 23 * scaleX)
+      .attr("y", posY + sizeY - graybar + 1)
+      .attr("width", 19 * scaleX)
+      .attr("height", 18 * scaleY)
+      .attr("fill", "white");
 
-		var cross = ramo.append('g').attr("class", "cross").attr("opacity", 0);
-		cross.append("path")
-			.attr("d", "M" + posX + "," + posY + "L" + (posX+sizeX*1.1) + "," + (posY+sizeY))
-			.attr("stroke", "#550000")
-			.attr("stroke-width", 9);
+    ramo
+      .append("text")
+      .attr("x", posX + sizeX * 1.2 - 17 * scaleX)
+      .attr("y", posY + sizeY - 6 * scaleY)
+      .text(self.creditos)
+      .attr("font-family", "sans-serif")
+      .attr("font-weight", "regular")
+      .attr("fill", "black")
+      .attr("font-size", 12 * scaleY);
 
-		// id
-		ramo.append("circle")
-			.attr("cx", posX + sizeX*1.2-10)
-			.attr("cy", posY + graybar/2 )
-			.attr("fill", "white")
-			.attr("r", 8);
-		ramo.append("text")
-			.attr("x", function() {
-				if (self.id > 9)
-					return posX + sizeX*1.2 - 10
-				return posX + sizeX*1.2 - 10.5
-			})
-			.attr("y", posY + graybar/2 + 3)
-			.attr("text-anchor", "middle")
-			.attr('font-family', 'sans-serif')
-			.attr("fill", "black")
-			.attr('font-size', 10)
-			.text(self.id);
+    // Nombre del ramo
+    ramo
+      .append("text")
+      .attr("x", posX + (sizeX * 1.2) / 2)
+      .attr("y", posY + sizeY / 2)
+      .text(self.nombre)
+      .attr("class", "ramo-label")
+      .attr("font-family", "sans-serif")
+      .attr("fill", function () {
+        if (self.sector != "IS") return "white";
+        return "#6D6E71";
+      })
+      .attr("font-size", 13)
+      .attr("text-anchor", "middle")
+      .attr("dy", 0);
 
-		// prerr circles!
-		let c_x = 0;
-		self.prer.forEach(function(p) {
-			let r = 9,
-			fontsize = 12,
-			variantX = 5
-			variantY = 5;
-			if (scaleX < 0.75) {
-				r--;
-				fontsize--;
-				variantX = 1;
-				variantY--;
-			}
-			ramo.append("circle")
-				.attr('cx', posX + r + c_x + variantX)
-				.attr('cy', posY + sizeY - graybar/2)
-				.attr('r', r)
-				.attr('fill', colorBySector[all_ramos[p].sector][0])
-				.attr('stroke', 'white');
-			ramo.append('text')
-				.attr('x', posX + r + c_x + variantX)
-				.attr('y', posY + sizeY - graybar/2 + variantY)
-				.text(all_ramos[p].id)
-				.attr("text-anchor", "middle")
-				.attr("font-family", "sans-serif")
-				.attr("font-size", fontsize)
-				.attr('fill', 'white');
-			c_x += r*2;
-		});
+    // Sigla
+    ramo
+      .append("text")
+      .attr("x", posX + 2)
+      .attr("y", posY + sizeY / 7)
+      .text(self.sigla)
+      .attr("font-family", "sans-serif")
+      .attr("font-weight", "bold")
+      .attr("fill", "white")
+      .attr("font-size", function () {
+        if (scaleX < 0.59) return 9;
+        return 12;
+      });
 
-		ramo.on('click', self.approveRamo);
+    // Capa transparente para bloquear si no tiene prerrequisitos cumplidos
+    ramo
+      .append("rect")
+      .attr("x", posX)
+      .attr("y", posY)
+      .attr("width", sizeX * 1.2)
+      .attr("height", sizeY)
+      .attr("fill", "white")
+      .attr("opacity", "0.001")
+      .attr("class", "non-approved");
 
-		return;
-	}
+    // Cruz de aprobado
+    var cross = ramo.append("g").attr("class", "cross").attr("opacity", 0);
+    cross
+      .append("path")
+      .attr(
+        "d",
+        "M" + posX + "," + posY + "L" + (posX + sizeX * 1.1) + "," + (posY + sizeY)
+      )
+      .attr("stroke", "#550000")
+      .attr("stroke-width", 9);
 
-	this.approveRamo = function() {
-			if (!approved) {
-				d3.select("#" + self.sigla).select(".cross").transition().delay(20).attr("opacity", "1");
-				APPROVED.push(self);
-			} else {
-				d3.select("#" + self.sigla).select(".cross").transition().delay(20).attr("opacity", "0.01");
-				let _i = APPROVED.indexOf(self)
-				if (_i > -1) {
-					APPROVED.splice(_i, 1);
-				}
-			}
-			approved = !approved;
-	}
+    // Circulito de ID del ramo
+    ramo
+      .append("circle")
+      .attr("cx", posX + sizeX * 1.2 - 10)
+      .attr("cy", posY + graybar / 2)
+      .attr("fill", "white")
+      .attr("r", 8);
 
-	this.verifyPrer = function() {
-		let _a = [];
-		APPROVED.forEach(function(ramo) {
-			_a.push(ramo.sigla);
-		});
-		_a = new Set(_a);
-		for(let r of self.prer) {
-			if (!_a.has(r)) {
-				ramo.select(".non-approved").transition().duration(70).attr("opacity", "0.71");
-				return;
-			}
-		}
-		ramo.select(".non-approved").transition().duration(70).attr("opacity", "0.0");
-	}
+    ramo
+      .append("text")
+      .attr("x", function () {
+        if (self.id > 9) return posX + sizeX * 1.2 - 10;
+        return posX + sizeX * 1.2 - 10.5;
+      })
+      .attr("y", posY + graybar / 2 + 3)
+      .attr("text-anchor", "middle")
+      .attr("font-family", "sans-serif")
+      .attr("fill", "black")
+      .attr("font-size", 10)
+      .text(self.id);
 
-	// Lo necesito
-	this.isApproved = function() {
-		return approved;
-	}
-// Supongo que tienes data_ELI cargado con la malla por semestre
-// y all_ramos es un objeto con claves las siglas y valores instancias Ramo
+    // Circulitos de prerrequisitos
+    let c_x = 0;
+    self.prer.forEach(function (p) {
+      let r = 9,
+        fontsize = 12,
+        variantX = 5,
+        variantY = 5;
+      if (scaleX < 0.75) {
+        r--;
+        fontsize--;
+        variantX = 1;
+        variantY--;
+      }
 
-function aprobarSemestre(semestre) {
-  if (!data_ELI[semestre]) {
-    console.warn("Semestre no encontrado:", semestre);
-    return;
-  }
-  // Limpiar todos los aprobados actuales
-  limpiarRamos();
+      const prereqRamo = all_ramos[p];
+      const prereqSector = prereqRamo ? prereqRamo.sector : self.sector;
 
-  // Marcar como aprobados los ramos del semestre
-  data_ELI[semestre].forEach(ramoData => {
-    const sigla = ramoData[1];
-    const ramo = all_ramos[sigla];
-    if (ramo && !ramo.isApproved()) {
-      ramo.approveRamo();
-    }
-  });
+      ramo
+        .append("circle")
+        .attr("class", "prer-circle")
+        .attr("data-sector", prereqSector)
+        .attr("cx", posX + r + c_x + variantX)
+        .attr("cy", posY + sizeY - graybar / 2)
+        .attr("r", r)
+        .attr("fill", colorBySector[prereqSector][0])
+        .attr("stroke", "white");
 
-  actualizarCreditos();
-}
+      ramo
+        .append("text")
+        .attr("x", posX + r + c_x + variantX)
+        .attr("y", posY + sizeY - graybar / 2 + variantY)
+        .text(prereqRamo ? prereqRamo.id : "?")
+        .attr("text-anchor", "middle")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", fontsize)
+        .attr("fill", "white");
 
-function configurarBotonesSemestre() {
-  const botones = document.querySelectorAll('.btn-semester');
-  botones.forEach(boton => {
-    boton.addEventListener('click', () => {
-      const sem = boton.getAttribute('data-semester');
-      aprobarSemestre(sem);
+      c_x += r * 2;
     });
-  });
-}
 
-// Ejecutar cuando todo esté cargado
-window.addEventListener('load', () => {
-  configurarBotonesSemestre();
-});
+    // Click en el ramo → aprobar / desaprobar
+    ramo.on("click", self.approveRamo);
+  };
 
+  // Marca / desmarca el ramo como aprobado
+  this.approveRamo = function () {
+    if (!approved) {
+      d3.select("#" + self.sigla)
+        .select(".cross")
+        .transition()
+        .delay(20)
+        .attr("opacity", "1");
+      APPROVED.push(self);
+    } else {
+      d3.select("#" + self.sigla)
+        .select(".cross")
+        .transition()
+        .delay(20)
+        .attr("opacity", "0.01");
+      let _i = APPROVED.indexOf(self);
+      if (_i > -1) {
+        APPROVED.splice(_i, 1);
+      }
+    }
+    approved = !approved;
+  };
 
+  // Verifica si se cumplen los prerrequisitos
+  this.verifyPrer = function () {
+    let _a = [];
+    APPROVED.forEach(function (ramo) {
+      _a.push(ramo.sigla);
+    });
+    _a = new Set(_a);
+    for (let r of self.prer) {
+      if (!_a.has(r)) {
+        ramo
+          .select(".non-approved")
+          .transition()
+          .duration(70)
+          .attr("opacity", "0.71");
+        return;
+      }
+    }
+    ramo
+      .select(".non-approved")
+      .transition()
+      .duration(70)
+      .attr("opacity", "0.0");
+  };
+
+  // Necesario para saber si está aprobado
+  this.isApproved = function () {
+    return approved;
+  };
 }
